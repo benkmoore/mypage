81f09db25f11a0b8f2c5bbe17796a2a0b5beee60
benkmoore <ben.karl.moore@gmail.com>
2021-01-28 16:27:59 +0000

    created navbar

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,17 @@
+export const MenuItems = [
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Projects',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Almanac',
+        url: '#',
+        cName: 'nav-links'
+    }
+]
